4b84ca52047e2fb7809bf689a4c90c5d6cd13ce4
ShanahElms <shanah1@live.com.au>
2022-12-06 08:05:33 +1000

    deploy test

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import {
   BrowserRouter,
   Routes,
   Route
-} from "react-router-dom"
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path = "/" element = {<App />}></Route>
+      <Route path = "/web_app" element = {<App />}></Route>
     </Routes>
   </BrowserRouter>
 );
